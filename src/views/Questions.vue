dfa048c6c675da0ac5c982ba7a208151b4f28698
Nathanne Isip <nathanneisip@gmail.com>
2024-04-05 01:43:08 +0800

    Improvements on Question page functionalities.

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -1,26 +1,47 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { useRouter } from "vue-router";
+
 import Question from "../components/Question.vue";
 
 const index = ref(0);
+const router = useRouter();
+
 const contents: Array<Array<string>> = [
     [
         "You, in the society",
         "What defines individuality, and how does it intersect with societal norms and expectations?",
         "assets/image-1.png",
-        "Assert"
+        "Assert",
+        "Go back"
     ],
     [
         "Optimistic Socio-culturism",
         "How do cultural and societal pressures shape our understanding of personal identity and sanity?",
         "assets/image-2.png",
-        "Proceed"
+        "Proceed",
+        "Return"
+    ],
+    [
+        "Soundness of Prudence",
+        "How do experiences of marginalization or oppression affect an individual's sense of sanity and identity?",
+        "assets/image-3.gif",
+        "Ok",
+        "Previous"
     ]
 ];
 
+function previous() {
+    if(index.value == 0) {
+        router.push("/");
+        return;
+    }
+
+    index.value--;
+}
+
 function next() {
     index.value++;
-    console.log(index.value);
 }
 </script>
 
@@ -32,7 +53,9 @@ function next() {
                 :title="contents[index][0]"
                 :message="contents[index][1]"
                 :banner="contents[index][2]"
-                :button="contents[index][3]"
+                :nextButton="contents[index][3]"
+                :previousButton="contents[index][4]"
+                :onPrevious="previous"
                 :onNext="next" />
         </div>
     </div>
